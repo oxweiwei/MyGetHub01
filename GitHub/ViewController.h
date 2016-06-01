5570eb975a9bbe6335b2b0297a08fef5318afb89
oxweiwei <student@ZXW.local>
2016-06-01 23:27:57 +0800

    oxweiwei

---
+++ GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHub/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitHub
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 ZXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"123456");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
